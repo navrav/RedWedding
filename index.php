717e447959073a2ffba65c9004f5d5bb23e17d33
DrSkulk <pichukim@hotmail.com>
2014-09-21 13:43:41 +1000

    Debug attempt 2

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
 	
 	<!-- Check submitted data -->
 	<script>
+	/*
 		$(document).ready(function() {
 			$("*").keyup(function() {
 				if ($("#user").val() != "" &&
@@ -53,6 +54,37 @@
 				});
 			});
 		});
+	*/
+	
+	// old code begin
+	$(document).ready(function(){
+	$("*").keyup(function(){
+	if($("#user").val()!="" && $("#pass").val()!="")
+	{
+	document.getElementById('log').disabled=false;
+	}else {
+	
+	document.getElementById('log').disabled=true;
+	}
+	});
+var log_out="<?=isset($_GET['log_out']) ? $_GET['log_out'] : '0'?>";
+if(log_out==1)$("#status").text('logout successful!').show();
+
+	$("#log").click(function(){
+
+$.get('ajax_check.php?t='+Math.random(),{pass:$("#pass").val(),user:$("#user").val()},function(checkresponse){
+if(checkresponse=="ok"){window.location.href='/feed.php';} //callback function, math.random here to wipe cache of IE explorer
+else{
+$("#status").text('Wrong Username or Password').show();
+//setTimeout(function(){$("#status").html('&nbsp;')},1500);
+}
+});
+		
+	});
+	
+	
+	});
+	// old code end
 	
 		function signup() {
 			window.location.href = '/signup.php';
