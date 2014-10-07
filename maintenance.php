5da09e417fd6759e67d8bbc0fe8ac18e5c9b3dae
niquitita <likelaika@gmail.com>
2014-10-07 23:07:40 +1000

    Maintenance page working
    Sends an email to 'aebspace@gmail.com' using Kevin's login details.
    Would be good if it had a popup saying the message had been sent.
    Currently redirects to the maintenance page again.

--- maintenance.php
+++ maintenance.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+//include('maintenance2.php');
 
 /*
 $to = 'corkisland@gmail.com';
@@ -28,40 +29,92 @@ if (mail($to, $subject, $message)){
 	echo'There was a problem sending the email.';
 	echo("<script>console.log('Houston we have a problem');</script>");
 }*/
-
-$from = 'email';
-$to = 'aebspace@gmai.com';
-$subject = 'AEBSpace Maintenance Issue Report';
-$msg = 'issue';
+// These were changed in sendmail
 
 
+   
+	   
 
-function sendmail($from,$to,$subject,$msg) {
-
-// Downloaded from https://github.com/PHPMailer/PHPMailer
-  include_once('inc/class.phpmailer.php');
-
-  $mail = new phpmailer();
-  $mail->SMTPDebug = 0;                            // debugging: 1 = errors and messages, 2 = messages only, 0 = off
-  $mail->IsSMTP();                                 // Set mailer to use SMTP
-  $mail->Host = 'mailhub.eait.uq.edu.au';                  // Specify server
-  $mail->Port = 25;                               // Server port: 465 ssl OR  587 tls  
-  //  $mail->SMTPSecure = 'tls';                       // Enable encryption, 'ssl' also accepted
-  $mail->SMTPAuth = false;                          // Enable SMTP authentication
-  $mail->Username = 's4204765@student.uq.edu.au';             // SMTP username
-  $mail->Password = 'tennis91';    // SMTP password
-  $mail->SetFrom($from,'MyApp');                   // Sender
-  $mail->AddReplyTo($from,'Support');              // Set an alternative reply-to address
-  $mail->AddAddress($to,'User');                   // Set who the message is to be sent to
-  $mail->Subject = $subject;                       // Set the subject lin// Prepares message for html (see doc for details http://phpmailer.worxware.com/?pg=tutorial)
-  $mail->MsgHTML($msg);// Send the message, check for errors 
-  $ok = $mail->Send(); 
-  return $ok;
+ // COMMENTED SAVED HERE
+// $from = 'nakedphatbear@gmail.com';
+// $to = 'aebspace@gmail.com';
+// $subject = 'AEBSpace Maintenance Issue Report';
+// $msg = 'issue';
+// 
+// 
+// 
+// function sendmail($from,$to,$subject,$msg) {
+//     echo("<script>console.log('launching sendmail function...');</script>");
+// // Downloaded from https://github.com/PHPMailer/PHPMailer
+//   require('inc/class.phpmailer.php');
+//   echo("<script>console.log('included phpmailer...');</script>");
+//   require_once('inc/class.smtp.php');
+//   echo("<script>console.log('included smtp...');</script>");
+// 
+//   $mail = new phpmailer();
+//   echo("<script>console.log('new email started...');</script>");
+//   $mail->SMTPDebug = false;                            // debugging: 1 = errors and messages, 2 = messages only, 0 = off
+//   $mail->IsSMTP();                                 // Set mailer to use SMTP
+//   $mail->Host = "mailhub.eait.uq.edu.au";                  // Specify server
+//   $mail->Port = 587;                               // Server port: 465 ssl OR  587 tls   //25
+//   $mail->SMTPSecure = 'tls';                       // Enable encryption, 'ssl' also accepted // commented
+//   $mail->SMTPAuth = true;                          // Enable SMTP authentication // false
+//   $mail->Username = 's4204765@student.uq.edu.au';             // SMTP username
+//   echo("<script>console.log('connected to host...');</script>");
   
-  }
-echo("<script>console.log('Loaded fine');</script>");
-sendmail($from,$to,$subject,$msg);
-echo("<script>console.log('Send happened');</script>");
+  ////// DON'T LOOK -- KEVIN'S PASSWORD
+  
+  																								$mail->Password = 'john.massey';    // SMTP password
+  
+  //////////////////// CAN LOOK AGAIN
+  
+  //$mail->SetFrom($from,'MyApp');                   // Sender
+  //$mail->AddReplyTo($from,'Support');              // Set an alternative reply-to address
+  
+  //$mail->AddAddress($to,'User');                   // Set who the message is to be sent to
+  
+  //$mail->Subject = $subject;                       // Set the subject lin// Prepares message for html (see doc for details http://phpmailer.worxware.com/?pg=tutorial)
+  //$mail->MsgHTML($msg);// Send the message, check for errors 
+  
+  // $mail->From = "nakedphatbear@gmail.com";
+//   $mail->AddAddress("aebspace@gmail.com");
+//   $mail->Subject = "Testing Email Functions";
+//   $mail->Body = "Check out my email!";
+//   
+//   if(!$mail->Send()){
+//   echo $mail->ErrorInfo;
+//   echo("<script>console.log('Message was not sent.');</script>");
+//   }else{
+//   echo("<script>console.log('Message was sent.');</script>");
+//   }
+//   
+  // echo("<script>console.log('About to send...');</script>");
+//     $ok = $mail->Send();  // Error is happening here
+// 	return $ok;
+	//$mail->Send(); // COMMENTED END HERE
+	  
+	  
+	  
+	  
+	  /*
+	if(!$ok) {
+    //echo ("<script>console.log('Message could not be sent.');</script>");
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+    //echo ("<script>console.log('Message has been sent');</script>";
+	}*/
+
+//echo("<script>console.log('Send happened2');</script>");  
+//  return $ok;
+  
+ //} isset ENDS HERE
+ 
+//echo("<script>console.log('Loaded finenew');</script>");
+//sendmail($from2,$to2,$subject2,$msg2);
+//echo("<script>console.log('Send happenednew');</script>");
+
+// Took out sendmail function
+
 /*
 if (sendmail($from,$to, $subject, $msg)){
 	echo 'Your message has been sent.';
@@ -73,15 +126,12 @@ if (sendmail($from,$to, $subject, $msg)){
 
 
 
-
-
 ?>
 
 <!DOCTYPE html>
 
 <html>
 	<head>
-		<title>AEB Space - Maintenance</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
 		<link rel="stylesheet" href="css/jquery.mobile-1.4.2.css">
@@ -92,14 +142,21 @@ if (sendmail($from,$to, $subject, $msg)){
 		
 		<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 		<script src="js/jquery.mobile-1.4.2.js"></script>
-		<script>
+		<!-- 
+<script>
 			function Submit() {
-				$.get("maintenance.php");
+				console.log('Called');
+				$.get("maintenance2.php");
+				console.log('After call');
 				//setTimeout(function() {window.location.href = "feed.php"}, 3000);
 				//document.getElementById('layover').style.display= "block";
 				//document.getElementById('confirmpop').style.display= "block";
 			}
-		</script>
+ -->
+		
+
+		
+		
 		<style>
 		
 		#header_brown {
@@ -178,11 +235,11 @@ if (sendmail($from,$to, $subject, $msg)){
 		                        	<p>
 		                        		If you have any issue regarding the maintenance of the building
 		                        		fill out the form below and an email will be send to the appropriate
-		                        		person. Testing.
+		                        		person.
 		                        	</p>
 		                        	<br><br>
 		                        	<p>
-		                        		<form action="maintenance.php">
+		                        		<form id="maintform" method="POST" action="maintenance2.php" name="email">
 		                        		
 		                        			First name: <input type="text" name="FirstName" ><br>
 											Last name: <input type="text" name="LastName" ><br>
@@ -191,7 +248,7 @@ if (sendmail($from,$to, $subject, $msg)){
 		                        			Issue: <textarea placeholder="Input a description of your issue here" name="issue" ></textarea>
 		                        			
 		                        			<br>
-		                        			<input type="button" value="Submit" onclick="Submit();"> 
+		                        			<input type="submit" value="Submit"> 
 		                        			
 		                        		</form>
 		                        		
@@ -220,6 +277,12 @@ if (sendmail($from,$to, $subject, $msg)){
 		        </div>
 		    </section>
   		</div> 
+  		<!--
+<?php
 
+echo("<script>console.log('Sending email...');</script>");
+sendmail($from, $to, $subject, $msg);
+echo("<script>console.log('Email sent...');</script>");
+?>-->
 	</body>
 </html>
---
+++ maintenance2.php
@@ -0,0 +1,198 @@
+<?php
+
+
+/*
+$to = 'corkisland@gmail.com';
+$subject = 'AEBSpace Maintenance Issue Report';
+
+$message = '<html><body>';
+//$message = 'Hello. This is a test message to see if this works.'
+
+
+$message = 'issue';
+$message = wordwrap($message, 70, "\r\n");
+//mail($to, $subject, $message);
+
+/*$smtp = Mail::factory('smtp', array(
+	'host' => 'ssl://smtp.gmail.com',
+	'port' => '465',
+	'auth' => true,
+	'username' => 'corkisland@gmail.com',
+	'password' => 'naveenkumar2014'
+));*/
+/*
+if (mail($to, $subject, $message)){
+	echo 'Your message has been sent.';
+	echo("<script>console.log('Your message has been sent');</script>");
+} else {
+	echo'There was a problem sending the email.';
+	echo("<script>console.log('Houston we have a problem');</script>");
+}*/
+// These were changed in sendmail
+// $from = 'email';
+// $to = 'aebspace@gmail.com';
+// $subject = 'AEBSpace Maintenance Issue Report';
+// $msg = 'issue';
+// 
+// 
+// 
+// //function sendmail($from,$to,$subject,$msg) {
+// 
+// // Downloaded from https://github.com/PHPMailer/PHPMailer
+//   include_once('inc/class.phpmailer.php');
+//   require_once('inc/class.smtp.php');
+// 
+//   $mail = new phpmailer();
+//   $mail->SMTPDebug = false;                            // debugging: 1 = errors and messages, 2 = messages only, 0 = off
+//   $mail->IsSMTP();                                 // Set mailer to use SMTP
+//   $mail->Host = 'mailhub.eait.uq.edu.au';                  // Specify server
+//   $mail->Port = 587;                               // Server port: 465 ssl OR  587 tls   //25
+//   $mail->SMTPSecure = 'tls';                       // Enable encryption, 'ssl' also accepted // commented
+//   $mail->SMTPAuth = true;                          // Enable SMTP authentication // false
+//   $mail->Username = 's4204765@student.uq.edu.au';             // SMTP username
+//   
+  
+  ////// DON'T LOOK -- KEVIN'S PASSWORD
+  
+  																								$mail->Password = 'john.massey';    // SMTP password
+  
+  //////////////////// CAN LOOK AGAIN
+  
+  //$mail->SetFrom($from,'MyApp');                   // Sender
+  //$mail->AddReplyTo($from,'Support');              // Set an alternative reply-to address
+  
+  //$mail->AddAddress($to,'User');                   // Set who the message is to be sent to
+  
+  //$mail->Subject = $subject;                       // Set the subject lin// Prepares message for html (see doc for details http://phpmailer.worxware.com/?pg=tutorial)
+  //$mail->MsgHTML($msg);// Send the message, check for errors 
+  
+	//$ok = $mail->Send();  // Error is happening here
+	
+	//$mail->Send();
+	  /*
+	if(!$ok) {
+    //echo ("<script>console.log('Message could not be sent.');</script>");
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+    //echo ("<script>console.log('Message has been sent');</script>";
+	}*/
+
+//echo("<script>console.log('Send happened2');</script>");  
+//  return $ok;
+  
+ // }
+//echo("<script>console.log('Loaded fine2nd');</script>");
+//sendmail($from2,$to2,$subject2,$msg2);
+//echo("<script>console.log('Send happened2nd');</script>");
+
+// Took out sendmail function
+
+/*
+if (sendmail($from,$to, $subject, $msg)){
+	echo 'Your message has been sent.';
+	echo("<script>console.log('Your message has been sent');</script>");
+} else {
+	echo'There was a problem sending the email.';
+	echo("<script>console.log('Houston we have a problem');</script>");
+}*/
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+$to = 'aebspace@gmail.com';
+$from = $_POST["email"];
+$name = $_POST["FirstName"] . " " . $_POST["LastName"];
+$subject = 'AEB Maintenance Issue';
+$issue = $_POST["issue"];
+//echo("<script>console.log('Sending to: '".$to.");</script>");
+//echo("<script>console.log('Sent from: '".$from.");</script>");
+//echo("<script>console.log('Subject: '".$subject.");</script>");
+//echo("<script>console.log('Message: '".$message.");</script>");
+
+
+function sendMail($to, $from, $subject, $issue, $name){
+include_once("inc/class.phpmailer.php");
+include("inc/class.smtp.php");
+$mail = new phpmailer();
+//echo "Created new class  <br>";
+$mail->IsSMTP();
+$mail->SMTPDebug=1;
+ini_set('display_errors', '1');
+
+//echo "Set SMTP  <br>";
+$mail->Host = "mailhub.eait.uq.edu.au";
+$mail->Port=25;
+//echo "Set Server  <br>";
+$mail->SMTPAuth = false;
+//$mail->SMTPSecure = 'ssl';
+$mail->Username = "s4204765@uq.edu.au";
+$mail->Password = "john.massey";
+$mail->From = 'aebspace@gmail.com';//$from;
+$mail->AddAddress($to);
+$mail->Subject = $subject;
+$mail ->AddAddress($from);
+$mail ->FromName = "AEBSpace Maintenance";
+
+
+
+/**
+$message = '<html><body>';
+			$message .= '<img src="http://css-tricks.com/examples/WebsiteChangeRequestForm/images/wcrf-header.png" alt="Website Change Request" />';
+			$message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+			$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_POST['req-name']) . "</td></tr>";
+			$message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['req-email']) . "</td></tr>";
+			$message .= "<tr><td><strong>Type of Change:</strong> </td><td>" . strip_tags($_POST['typeOfChange']) . "</td></tr>";
+			$message .= "<tr><td><strong>Urgency:</strong> </td><td>" . strip_tags($_POST['urgency']) . "</td></tr>";
+			$message .= "<tr><td><strong>URL To Change (main):</strong> </td><td>" . $_POST['URL-main'] . "</td></tr>";
+			$addURLS = $_POST['addURLS'];
+			if (($addURLS) != '') {
+			    $message .= "<tr><td><strong>URL To Change (additional):</strong> </td><td>" . strip_tags($addURLS) . "</td></tr>";
+			}
+			$curText = htmlentities($_POST['curText']);           
+			if (($curText) != '') {
+			    $message .= "<tr><td><strong>CURRENT Content:</strong> </td><td>" . $curText . "</td></tr>";
+			}
+			$message .= "<tr><td><strong>NEW Content:</strong> </td><td>" . htmlentities($_POST['newText']) . "</td></tr>";
+			$message .= "</table>";
+			$message .= "</body></html>";**/
+
+
+
+$mail -> IsHTML(true);
+$message = '<body>'; // '<html><body>';
+$message .="<strong>AEB Maintenance Email Report</strong>";
+$message .='<table cellpadding="10">';
+$message .="<tr><td><strong>Name:</strong></td><td>" . $name . "</td></tr>";
+$message .="<tr><td><strong>Email:</strong></td><td>" . $from . "</td></tr>";
+$message .="<tr><td><strong>Issue:</strong></td><td>" . $issue . "</td></tr>";
+$message .="</table>";
+$message .="</body>"; // "</html></body>";
+
+// Firstname, lastname, email, issue
+
+$mail->Body = $message;
+//echo "Set message contents <br> ";
+;
+if(!$mail->Send()){
+echo 'Message not sent.';
+echo 'Mailer error: '.$mail->ErrorInfo;
+}else{
+echo 'Message was sent.';
+}  
+}
+
+
+sendMail($to, $from, $subject, $issue, $name);
+
+header( 'Location: maintenance.php' ) ;
+
+
+
+
+
+
+
+
+
+?>
