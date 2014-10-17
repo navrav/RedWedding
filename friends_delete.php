46d9a5023664e7b88d82b204d6a116e025d80a1f
EvelynGuo <guoyuqing0827@live.cn>
2014-10-17 16:03:05 +1000

    delete friends fixing

--- friends_delete.php
+++ friends_delete.php
@@ -8,7 +8,8 @@ $delete_id=$_REQUEST['delete_id'];//friends who the user wants to delete
 
 //execute sql query, satisfy two conditions to delete, the user and the user's friends  
 
-$sql="delete from `Friends` where `ID_2`='".$delete_id."' and `ID_1`='".$u_id."'";
+$sql1="delete from `Friends` where `ID_2`='".$delete_id."' and `ID_1`='".$u_id."'";
+$sql2="delete from `Friends` where `ID_2`='".$u_id."' and `ID_1`='".$delete_id."'";
 
 
-if(mysqli_query($dbconn, $sql)) echo 'ok';//if execute successfully, echo ok
+if(mysqli_query($dbconn, $sql1) && mysqli_query($dbconn, $sql2)) echo 'ok';//if execute successfully, echo ok
