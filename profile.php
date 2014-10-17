f61708b13f3ee22a5e8fbd685ffcccce579dca94
EvelynGuo <guoyuqing0827@live.cn>
2014-10-17 13:06:14 +1000

    add avatar question mark

--- profile.php
+++ profile.php
@@ -32,11 +32,11 @@
 	
 	$("#male_img").find('img').bind("click",function(){ 
 	
-	
+	if($(this).index()<2){
 	$.get('change_pic.php?t='+Math.random(),{user:"<?=$user?>",pic:$(this).attr('data_src')},function(change_result){
 	
 	
-	if(change_result!=''){//change the avatar once the database changes successfully
+	if(change_result!=''){//change the database then change the avatar
 	
 	$("#show_img").attr({'src':'avatars/'+change_result});
 	  
@@ -46,7 +46,7 @@
 	
 	});  
 	
-	
+	}
 	  
 	   });
 	
@@ -80,7 +80,7 @@
                     <div class="col-md-8 col-md-offset-2">
                         <div class="">
                         	<div class="col-md-4 col-md-offset-4">
-                        	<img id="show_img" src="avatars/<?=!empty($currentUser['pic']) ? $currentUser['pic'] : "m.png"?>" width="100px" height="105px" class="img-circle"/>
+                        	<img id="show_img" src="avatars/<?=!empty($currentUser['pic']) ? $currentUser['pic'] : "qm.png"?>" width="100px" height="105px" class="img-circle"/>
 							<!-- The below two lines are coming from top banner. -->
                         	<button id="change_img"> <?php //echo $fname ." ". $lname;
 							echo "Change Avatar";
@@ -151,7 +151,7 @@
 	<div id="select_img" style="display:none; position:fixed; left:50%; top:50%;margin-left:-125px;margin-top:-74.5px; "> 
 		<div class="modal-content" style="background-color:#262626; width:250px">
 				<div class="modal-body"  style="padding: 15px; padding-bottom: 5px;">
-					<p>Change your hat?</p>
+					<p>change your head portrait?</p>
 				</div>
 			
 				<div class="modal-footer" style="padding-top: 10px; padding-bottom: 10px;">
@@ -160,14 +160,14 @@
 			
 				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/hat1.png" data_src="m1.png">
 				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/hat2.png" data_src="m2.png">
-				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/hat3.png" data_src="m3.png">
-				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/hat4.png" data_src="m4.png">
-				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/hat5.png" data_src="m5.png">
-				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/hat6.png" data_src="m6.png">
-				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/hat7.png" data_src="m7.png">
-				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/hat8.png" data_src="m8.png">
-				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/hat9.png" data_src="m9.png">
-				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/hat10.png" data_src="m10.png"></div>
+				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/qm.png" data_src="m.png">
+				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/qm.png" data_src="m.png">
+				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/qm.png" data_src="m.png">
+				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/qm.png" data_src="m.png">
+				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/qm.png" data_src="m.png">
+				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/qm.png" data_src="m.png">
+				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/qm.png" data_src="m.png">
+				<img style="max-height:40px;float:left;cursor:pointer;" src="avatars/qm.png" data_src="m.png"></div>
 					<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
 					
 					
