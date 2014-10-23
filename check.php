67a36c0995ffb18df8a3a56a89ea5b6b072d3472
navrav <mystero5656@hotmail.com>
2014-10-23 22:56:07 +1000

    Adding Check

---
+++ check.php
@@ -0,0 +1,40 @@
+<?php
+// Previously known as "ajax_check.php"
+
+session_start();
+include_once("servercon.php");
+include_once("PasswordHash.php");
+
+
+//Use this for security -- not yet implemented
+//$hasher = new PasswordHash(8, FALSE);
+//$hash   = $hasher->HashPassword($password);
+
+
+// username and password sent from form
+$myusername= mysqli_real_escape_string($dbconn, $_POST['user']);
+$mypassword=mysqli_real_escape_string($dbconn, $_POST['pass']);
+
+
+$query = "SELECT `u_ID`, `pass` FROM `Users` WHERE email= ?";
+
+$stmt = mysqli_prepare($dbconn, $query);
+
+mysqli_stmt_bind_param($stmt, 's', $myusername);
+
+mysqli_stmt_execute($stmt);
+
+mysqli_stmt_bind_result($stmt, $uID, $pass);
+
+if (mysqli_stmt_fetch($stmt)){
+	if (validate_password($mypassword, $pass)){
+		$_SESSION['username'] = $uID;
+    	$_SESSION['loggedIn'] = true;
+   		header('Location: /feed.php');
+	} else {
+		header('Location: /index.php?logfailed=1');
+	}
+} else {
+	header('Location: /index.php?logfailed=1');
+}	
+?>
