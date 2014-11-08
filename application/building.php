d1a388f48532f7302ce9bc66166dab42c8e7ae5a
navrav <mystero5656@hotmail.com>
2014-11-08 23:22:07 +1000

    Styling for Building.php

--- application/building.php
+++ application/building.php
@@ -35,9 +35,9 @@
 		
 		<!--top bar-->
 		<div data-role="page" data-theme="b" style="background-color:white;">
-		  <div data-role="header" id="header_green">
-		    <?php require("topbanner.php"); ?>
-		</div>
+			<div data-role="header" id="header_green">
+		    	<?php require("topbanner.php"); ?>
+			</div>
 		<!--end top bar-->
   
   
@@ -52,10 +52,8 @@
 		            <div class="container">
 		                <div class="row">
 		                    <div class="col-md-8 col-md-offset-2">
-		                        <div class="">
-		                        	
-		                        	<img src="images/aeb_bw.jpg" class="img-responsive"/>
-		                        	<div id="info_text">
+	                        	<img src="images/aeb_bw.jpg" class="img-responsive"/>
+	                        	<div id="info_text">
 		                        	<h4>ADVANCE ENGINEERING BUILDING</h4>
 		                        	<h5> Building 49</h5>
 		                        	<h5>Univeristy of Queensland</h5>
@@ -64,23 +62,23 @@
 		                        	<p>7:00 am - 18:00pm<br>
 		                        		Every Day
 		                        	</p>
-		                        	<p>The Advanced Engineering Building (AEB) at
-					the University of Queensland is a state-of-the-art 
-					engineering education building with flexible 
-					teaching and learning spaces. The multi-purpose 
-					building has the appropriate mix of learning, 
-					workplace and social areas. The building 
-					co-locates five key materials, science and 
-					engineering research centres and occupies a 
-					prime site overlooking the University lakes.</p>
-		                        	</div>
-		                         </div>
+		                        	<p>
+		                        	The Advanced Engineering Building (AEB) at
+									the University of Queensland is a state-of-the-art 
+									engineering education building with flexible 
+									teaching and learning spaces. The multi-purpose 
+									building has the appropriate mix of learning, 
+									workplace and social areas. The building 
+									co-locates five key materials, science and 
+									engineering research centres and occupies a 
+									prime site overlooking the University lakes.
+									</p>
+	                        	</div>
 		                    </div>
 		                </div>
 		            </div>
 		        </div>
 		    </section>
   		</div> 
-
 	</body>
 </html>
